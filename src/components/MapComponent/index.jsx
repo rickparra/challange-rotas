641831f63b229fb75e893731cabd7baa2b063373
Parra <hpbenitez7@gmail.com>
2023-09-14 15:34:33 -0300

    arrumando mapa

--- src/components/MapComponent/index.jsx
+++ src/components/MapComponent/index.jsx
@@ -1,10 +1,29 @@
-
-import styles from './MapContent.css'
+import React from 'react';
 
 export default function MapContent() {
+  const mapContainerStyle = {
+    width: '100%',
+    height: '100%',
+    border: '1px solid #ccc',
+    borderRadius: '5px',
+    overflow: 'hidden',
+  };
+
+  const iframeStyle = {
+    width: '100%',
+    height: '100%',
+    border: 'none',
+  };
+
   return (
-    <>
-      <img src="paulista-mapa.png" alt="Mapa Paulista" />
-    </>
+    <div style={mapContainerStyle}>
+      <iframe
+        src="plotly.html"
+        frameBorder="0"
+        scrolling="no"
+        allowFullScreen
+        style={iframeStyle}
+      ></iframe>
+    </div>
   );
 }
