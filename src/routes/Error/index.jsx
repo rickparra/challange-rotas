12d691271c4e3e1b86453aaeb9fa23d52835efda
TonyWillianFIAP <rm550667@fiap.com.br>
2023-11-09 14:16:34 -0300

    Correção de página de erro

--- src/routes/Error/index.jsx
+++ src/routes/Error/index.jsx
@@ -2,6 +2,7 @@ import styles from "./error.module.scss"
 import erro_404 from "../../assets/erro_404.png"
 import BotaoPrincipal from "../../components/BotaoPrincipal"
 import { useNavigate } from "react-router-dom"
+//Página padrão de erro
 
 export default function Error() {
     const navegar = useNavigate();
